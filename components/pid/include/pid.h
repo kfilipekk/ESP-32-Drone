16f3e910f183d36d83fa3f66042e2b42b33f9467
Krystian Filipek <kf446@cam.ac.uk>
2025-12-15 08:10:28 +0000

    Add debug accessors to PID controller and apply formatting standards

--- components/pid/include/pid.h
+++ components/pid/include/pid.h
@@ -13,13 +13,26 @@ public:
 
     void set_gains(float kp, float ki, float kd);
 
+    float get_kp() const { return kp_; }
+    float get_ki() const { return ki_; }
+    float get_kd() const { return kd_; }
+    
+    //debug getters
+    float get_last_p() const { return last_p_; }
+    float get_last_i() const { return integral_; } //integral storage
+    float get_last_d() const { return last_d_; }
+
+private:
+    float kp_;
+    float ki_;
+    float kd_;
+    
     float integral_;
     float prev_error_;
     
     float output_limit_;
     float integral_limit_;
     float dt_;
-
     //debug storage
     float last_p_ = 0.0f;
     float last_d_ = 0.0f;
